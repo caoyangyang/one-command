a8bb78c6b8903e476015231c6007e2500a933c5a
TW-DEV-YangyangCAO <yycao@thoughtworks.com>
2019-09-16 16:41:55 +0800

    feat: add build image step

--- plugins/jenkins.js
+++ plugins/jenkins.js
@@ -5,14 +5,16 @@ const runExec = require('../common/run-exec');
 
 exports.exec = async () => {
     runExec("cp ./template/init-demo/Jenkinsfile ./Jenkinsfile")
-    getUrlFromInput((url)=>generateJenkinsFileFor(url))
+    runExec("mkdir -p ./docker")
+    runExec("cp ./template/init-demo/Dockerfile ./docker/Dockerfile")
+    getUrlFromInput((url, image_name)=>generateJenkinsFileFor(url,image_name))
 }
 
-async function generateJenkinsFileFor(url) {
+async function generateJenkinsFileFor(url,image_name) {
     const options = {
         files: './Jenkinsfile',
-        from: /github_url/g,
-        to: url ,
+        from: [/github_url/g, /image_name/g],
+        to: [url, image_name],
     };
 
     try {
@@ -26,11 +28,12 @@ async function generateJenkinsFileFor(url) {
 
 function getUrlFromInput(callback){
     prompt.start();
-    prompt.get(['github_url'], function (err, result) {
+    prompt.get(['github_url','image_name'], function (err, result) {
         if (err) {
             console.error(chalk.green(err));
         }else {
-            callback(`'${result.github_url}'`);
+            console.log("result",result);
+            callback(`'${result.github_url}'`,`'${result.image_name}'`);
         }
     });
 }
